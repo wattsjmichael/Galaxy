44e2fbaba67b1ab4857fbd300d9ab6e57ed8ce8a
Michael Watts <wattsjmichael@gmail.com>
2020-09-04 08:31:06 -0700

    adds dependencies for project

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlwebpackPlugin = require('html-webpack-plugin');
+const { CleanwebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'eval-source-map',
+  devServer: {               
+    contentBase: './dist'    
+  },
+  plugins: [
+    new CleanwebpackPlugin(),
+    new HtmlwebpackPlugin({
+      title: 'Shape Tracker',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader"
+      }
+    ]
+  }
+};
