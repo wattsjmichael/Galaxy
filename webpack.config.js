3cdc02c586dbd4231701b978da15eeb4cff72abf
Michael Watts <wattsjmichael@gmail.com>
2020-09-04 08:52:06 -0700

    Server is not working, creating a new repo and starting over

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,43 @@
 const path = require('path');
-const HtmlwebpackPlugin = require('html-webpack-plugin');
-const { CleanwebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 
 module.exports = {
-  entry: './src/main.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  devtool: 'eval-source-map',
-  devServer: {               
-    contentBase: './dist'    
-  },
-  plugins: [
-    new CleanwebpackPlugin(),
-    new HtmlwebpackPlugin({
-      title: 'Shape Tracker',
-      template: './src/index.html',
-      inject: 'body'
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "eslint-loader"
-      }
-    ]
-  }
+entry: './src/main.js',
+output: {
+  filename: 'bundle.js',
+  path: path.resolve(__dirname, 'dist')
+},
+devtool: 'eval-source-map',
+devServer: {
+  contentBase: './dist'
+},
+plugins: [
+  new CleanWebpackPlugin(),
+  new HtmlWebpackPlugin({
+    title: 'My Project',
+    template: './src/index.html',
+    inject: 'body'
+  })
+],
+module: {
+  rules: [
+    {
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader'
+      ]
+    },
+    {
+      test: /\.js$/,
+      exclude: [
+          /node_modules/,
+          /spec/
+        ],
+      loader: "eslint-loader"
+    }
+  ]
+}
 };
